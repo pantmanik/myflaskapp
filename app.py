ae8aa35ce500208573cdd99d52b8cb8476eb80eb
pantmanik <pantmanik@gmail.com>
2018-04-29 20:59:20 +0530

    Final

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, flash,request, redirect, url_for, session,logging
-from data import Articles
+#from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
@@ -17,7 +17,7 @@ app.config['MYSQL_CURSORCLASS']='DictCursor'
 #init mysql
 mysql = MySQL(app)
 
-Articles=Articles()
+#Articles=Articles()
 
 @app.route('/')
 def index():
@@ -29,11 +29,30 @@ def about():
 
 @app.route('/articles')
 def articles():
-	return render_template('articles.html', articles=Articles)
+	cur=mysql.connection.cursor()
+
+	result=cur.execute("SELECT * FROM articles")
+
+	articles=cur.fetchall()
+
+	if result>0:
+		return render_template("articles.html", articles=articles)
+	else:
+		msg = "No Articles Found"		
+		return render_template('articles.html', msg=msg)
+
+	cur.close()
 
 @app.route('/article/<string:id>')
 def article(id):
-	return render_template('article.html', id=id)
+	cur=mysql.connection.cursor()
+
+	result=cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+	article=cur.fetchone()
+
+	cur.close()
+	return render_template('article.html', article=article)
 
 class RegisterForm(Form):
 	name=StringField('Name', [validators.Length(min=1, max=50)])
@@ -117,9 +136,21 @@ def logout():
 @app.route('/dashboard')
 @is_logged_in
 def dashboard():
-	return render_template('dashboard.html')
+	cur=mysql.connection.cursor()
 
-#ARticle Form Class
+	result=cur.execute("SELECT * FROM articles WHERE author=%s", [session['username']])
+
+	articles=cur.fetchall()
+
+	if result>0:
+		return render_template("dashboard.html", articles=articles)
+	else:
+		msg = "No Articles Found"		
+		return render_template('dashboard.html', msg=msg)
+
+	cur.close()
+
+#Article Form Class
 class ArticleForm(Form):
 	title=StringField('Title', [validators.Length(min=1, max=200)])
 	body = TextAreaField('Body', [validators.Length(min=4)])
@@ -143,6 +174,50 @@ def add_article():
 
 	return render_template('add_article.html', form=form)
 
+@app.route('/edit_article/<string:id>', methods=['GET', 'POST'])
+@is_logged_in
+def edit_article(id):
+	cur=mysql.connection.cursor()
+
+	result=cur.execute("SELECT * FROM articles WHERE id = %s", [id])
+
+	article = cur.fetchone()
+
+	form=ArticleForm(request.form)
+
+	form.title.data=article['title']
+	form.body.data=article['body']
+
+	if request.method=='POST' and form.validate():
+		title=request.form['title']
+		body=request.form['body']
+		
+		cur=mysql.connection.cursor()
+		cur.execute("UPDATE articles SET title=%s, body=%s WHERE id = %s", (title,body,id))
+		mysql.connection.commit()
+		cur.close()
+
+		flash('Article Updated!', 'success')
+
+		return redirect(url_for('dashboard'))
+
+	return render_template('edit_article.html', form=form)
+
+@app.route('/delete_article/<string:id>', methods=['POST'])
+@is_logged_in
+def delete_article(id):
+
+	cur=mysql.connection.cursor()
+
+	cur.execute("DELETE FROM articles WHERE id = %s", [id])
+
+	mysql.connection.commit()
+
+	cur.close()
+
+	flash("Article Deleted!", "success")
+
+	return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
 	app.secret_key='secret123'
